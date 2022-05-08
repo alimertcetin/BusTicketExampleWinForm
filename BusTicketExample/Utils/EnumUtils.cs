fe1d952afc58e8eb2059f0c492274324788f1707
alimertcetin <mrtmert54@gmail.com>
2022-05-08 16:33:44 +0300

    Imported basic form utils

---
+++ BusTicketExample/Utils/EnumUtils.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace XIV.Utils
+{
+    public static class EnumUtils
+    {
+        public static string GetTypeString<T>(object value) where T : Enum
+        {
+            return Enum.GetName(typeof(T), value);
+        }
+
+        public static T GetType<T>(string value) where T : Enum
+        {
+            Array values = Enum.GetValues(typeof(T));
+            foreach (var item in values)
+            {
+                if (item.ToString() == value)
+                {
+                    return (T)item;
+                }
+            }
+            return default(T);
+        }
+
+        public static Array GetValues<T>() where T : Enum
+        {
+            return Enum.GetValues(typeof(T));
+        }
+    }
+}
