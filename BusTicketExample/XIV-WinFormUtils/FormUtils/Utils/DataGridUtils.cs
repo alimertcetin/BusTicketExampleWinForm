10250aa22e5ffb0a9800f1f107820901da9d1824
alimertcetin <mrtmert54@gmail.com>
2022-05-14 12:16:39 +0300

    Revert "Submodule dir changes"
    This reverts commit b75bcadd84b0636be77d2204020add77528c2e6e.

---
+++ BusTicketExample/XIV-WinFormUtils/FormUtils/Utils/DataGridUtils.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows.Forms;
+
+namespace XIV.Utils
+{
+    /// <summary>
+    /// Utilities for <see cref="DataGridView"/> class
+    /// </summary>
+    public static class DataGridUtils
+    {
+        /// <summary>
+        /// Creates a new column with giving <paramref name="DataPropertyName"/> and <paramref name="Header"/> 
+        /// and returns it
+        /// </summary>
+        /// <param name="DataPropertyName"></param>
+        /// <param name="Header"></param>
+        /// <returns>A new <see cref="DataGridViewColumn"/></returns>
+        public static DataGridViewColumn CreateColumn(string DataPropertyName, string Header)
+        {
+            DataGridViewColumn column = new DataGridViewTextBoxColumn();
+            column.Name = DataPropertyName;
+            column.DataPropertyName = DataPropertyName;
+            column.HeaderText = Header;
+            return column;
+        }
+    }
+
+}
