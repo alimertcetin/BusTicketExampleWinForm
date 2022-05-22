09ff2a740d96f45b8820e37b3ec05222dffaa497
alimertcetin <mrtmert54@gmail.com>
2022-05-22 03:33:19 +0300

    Added a new info label

--- BusTicketExample/Forms/frm_CreateBusLayout.Designer.cs
+++ BusTicketExample/Forms/frm_CreateBusLayout.Designer.cs
@@ -35,20 +35,22 @@ namespace BusTicketExample.Forms
             this.txt_SizeX = new System.Windows.Forms.TextBox();
             this.txt_SizeY = new System.Windows.Forms.TextBox();
             this.btn_ApplySize = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // txt_Log
             // 
-            this.txt_Log.Location = new System.Drawing.Point(547, 12);
+            this.txt_Log.Location = new System.Drawing.Point(597, 40);
             this.txt_Log.Multiline = true;
             this.txt_Log.Name = "txt_Log";
-            this.txt_Log.Size = new System.Drawing.Size(240, 191);
+            this.txt_Log.ReadOnly = true;
+            this.txt_Log.Size = new System.Drawing.Size(190, 62);
             this.txt_Log.TabIndex = 0;
             // 
             // lbl_SizeX
             // 
             this.lbl_SizeX.AutoSize = true;
-            this.lbl_SizeX.Location = new System.Drawing.Point(547, 222);
+            this.lbl_SizeX.Location = new System.Drawing.Point(655, 112);
             this.lbl_SizeX.Name = "lbl_SizeX";
             this.lbl_SizeX.Size = new System.Drawing.Size(37, 15);
             this.lbl_SizeX.TabIndex = 1;
@@ -57,7 +59,7 @@ namespace BusTicketExample.Forms
             // lbl_SizeY
             // 
             this.lbl_SizeY.AutoSize = true;
-            this.lbl_SizeY.Location = new System.Drawing.Point(626, 222);
+            this.lbl_SizeY.Location = new System.Drawing.Point(734, 112);
             this.lbl_SizeY.Name = "lbl_SizeY";
             this.lbl_SizeY.Size = new System.Drawing.Size(37, 15);
             this.lbl_SizeY.TabIndex = 2;
@@ -65,33 +67,43 @@ namespace BusTicketExample.Forms
             // 
             // txt_SizeX
             // 
-            this.txt_SizeX.Location = new System.Drawing.Point(547, 240);
+            this.txt_SizeX.Location = new System.Drawing.Point(655, 130);
             this.txt_SizeX.Name = "txt_SizeX";
             this.txt_SizeX.Size = new System.Drawing.Size(53, 23);
             this.txt_SizeX.TabIndex = 3;
             // 
             // txt_SizeY
             // 
-            this.txt_SizeY.Location = new System.Drawing.Point(626, 240);
+            this.txt_SizeY.Location = new System.Drawing.Point(734, 130);
             this.txt_SizeY.Name = "txt_SizeY";
             this.txt_SizeY.Size = new System.Drawing.Size(53, 23);
             this.txt_SizeY.TabIndex = 4;
             // 
             // btn_ApplySize
             // 
-            this.btn_ApplySize.Location = new System.Drawing.Point(698, 222);
+            this.btn_ApplySize.Location = new System.Drawing.Point(655, 174);
             this.btn_ApplySize.Name = "btn_ApplySize";
-            this.btn_ApplySize.Size = new System.Drawing.Size(89, 47);
+            this.btn_ApplySize.Size = new System.Drawing.Size(134, 47);
             this.btn_ApplySize.TabIndex = 5;
             this.btn_ApplySize.Text = "Apply Size";
             this.btn_ApplySize.UseVisualStyleBackColor = true;
             this.btn_ApplySize.Click += new System.EventHandler(this.btn_ApplySize_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(597, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(134, 15);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Press R to change mode";
             // 
             // frm_CreateBusLayout
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(799, 404);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.btn_ApplySize);
             this.Controls.Add(this.txt_SizeY);
             this.Controls.Add(this.txt_SizeX);
@@ -115,5 +127,6 @@ namespace BusTicketExample.Forms
         private System.Windows.Forms.TextBox txt_SizeX;
         private System.Windows.Forms.TextBox txt_SizeY;
         private System.Windows.Forms.Button btn_ApplySize;
+        private System.Windows.Forms.Label label1;
     }
 }
